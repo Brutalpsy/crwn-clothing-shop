6101bf4a8b2e75b45af874568672a266633afc15
psy <brutalpsy89@gmail.com>
2020-04-24 23:39:37 +0200

    added menu items and some basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import HomePage from './pages/homepage/homepage.componenet';
 import './App.css';
-import HomePage from './homepage.component';
+import { Route, Switch, Link } from 'react-router-dom';
+
+const HatsPage = (props) => (
+  <div>
+    <Link to='/topics'>Topics</Link>
+    <button onClick={() => props.history.push('/topics')}>goto</button>
+    <h1>Hats page</h1>
+  </div>
+);
+
+const TopicsList = (props) => (
+  <div>
+    <button onClick={() => props.history.push('/hatspage')}>hats</button>
+    <h1>Topic list</h1>
+    <Link to={`${props.match.url}/13`}>to 13</Link> <br />
+    <Link to={`${props.match.url}/14`}>to 14</Link> <br />
+    <Link to={`${props.match.url}/15`}>to 15</Link> <br />
+  </div>
+);
+
+const TopicDetail = (props) => (
+  <div>
+    <h1>Topic detail</h1>
+    <button onClick={() => props.history.push('/topics')}>back</button>
+  </div>
+);
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hatspage' component={HatsPage} />
+      <Route exact path='/topics' component={TopicsList} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
+    </Switch>
   );
 }
 
