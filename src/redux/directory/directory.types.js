b0e29e8b72adfc1904a632d252a80eacc5ba6aed
psy <brutalpsy89@gmail.com>
2020-05-10 22:32:28 +0200

    separating cra app into client folder, added express server for stripe payments

--- src/redux/directory/directory.types.js
+++
@@ -1,2 +0,0 @@
-const directoryActionTypes = {};
-export default directoryActionTypes;
--- src/redux/user/user.selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const userState = (state) => state.user;
-
-export const selectCurrentUser = createSelector(
-  [userState],
-  (user) => user.currentUser
-);
