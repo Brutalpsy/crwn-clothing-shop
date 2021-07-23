fbf3bc9115042fd3bde63c27e4da45488ef6f531
psy <brutalpsy89@gmail.com>
2021-07-24 00:42:24 +0200

    changed name service-worker import

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <Provider store={store}>
